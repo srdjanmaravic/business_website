bf66f6922db49e2f029497c7c17a5cfd1460823d
Srdjan Maravic <srdjanmaravic@gmail.com>
2021-03-12 19:53:38 +0100

    update

---
+++ partials/footer.php
@@ -0,0 +1,25 @@
+ <!-- Footer -->
+ <footer>
+        <div class="container">
+          <div class="col">
+          <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; Consalting</span></a>
+          </div>
+          <div class="col">
+              <i class="fas fa-map-marker-alt"></i>
+              <p>Main Street 2, Novi Sad </p>
+              <p>Serbia</p>
+          </div>
+          <div class="col">
+          <i class="fas fa-envelope"></i>
+              <p>office@srki.rs</p>
+              <p>info@srdjan.rs</p>
+          </div>
+          <div class="col">
+                <i class="fas fa-phone"></i>
+              <p>Main Street 2, Novi Sad </p>
+              <p>Serbia</p>
+          </div>
+        </div>
+    </footer>
+</body>
+</html>
