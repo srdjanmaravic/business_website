bb17c7eed7adf369510d02511d3478c775b3624f
Srdjan Maravic <srdjanmaravic@gmail.com>
2021-03-12 21:24:29 +0100

    about

--- index.php
+++ index.php
@@ -9,6 +9,27 @@
     <button>About us</button>
 </header>
    
+<!-- Headings -->
+<section class="container headings">
+    <article>
+        <h2>01</h2>
+    </article>
+    <article>
+        <h3>About us</h3>
+        <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </article>
+</section>
+
+<section class="about">
+    <article>
+        <img src="img/about.jpg" alt="">
+    </article>
+    <article>
+        <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>
+    </article>
+</section>
 
 
 <?php include "partials/footer.php" ?>
