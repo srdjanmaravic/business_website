856d0edf0a3bb4e73aed5439617d4eba7e93e9b2
Srdjan Maravic <srdjanmaravic@gmail.com>
2021-03-12 21:58:04 +0100

    business

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <!-- Header -->
 
-<header>
+<header class="mainHeader">
     <h1>Trusted and <br> professionals advisers</h1>
     <h3>For your business</h3>
     <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
@@ -31,5 +31,87 @@
     </article>
 </section>
 
+<!-- Headings -->
+<section class="container headings">
+    <article>
+        <h2>02</h2>
+    </article>
+    <article>
+        <h3>Services</h3>
+        <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </article>
+</section>
+ <!-- services -->
+ <section class="services container">
+        <article>
+            <i class="far fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-filter"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullhorn"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+    </section>
+
+    <section class="business">
+        <header class="container headings black">
+            <article>
+                <h2>03</h2>
+            </article>
+            <article>
+                <h3>Services</h3>
+                <h2><span>HOW TO PROVIDE</span> TOOL THAT HELP ANYONE</h2>
+                <h4>Get your Business in 4 easy steps</h4>
+                <button>About us</button>
+            </article>
+        </header>
+        <div class="counter container">
+            <div class="col">
+                <h2>18</h2>
+                <h4>NOMINATIONS</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>5</h2>
+                <h4>AWARDS</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>45</h2>
+                <h4>AGENCIES</h4>
+                <hr>
+            </div>
+        </div>
+</section>
+   
 
 <?php include "partials/footer.php" ?>
